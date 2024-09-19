0701f437212b48e52b6c2524a434c245160c74db
saikrish1995 <potlasaikrishna@gmail.com>
2024-09-19 13:14:49 +0100

    expense_mysql

--- mysql.sh
+++ mysql.sh
@@ -46,5 +46,10 @@ VALIDATE $? "enabling mysqld"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "starting mysqld"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
-VALIDATE $? "setting root password"
+mysql -h 172.31.105.220 -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "$R Root password is not set $N, setting up now." | tee -a $LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
+fi
+
