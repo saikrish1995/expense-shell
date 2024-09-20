2e8a6f032528afc8eb118e31518302c1b246e43e
saikrish1995 <potlasaikrishna@gmail.com>
2024-09-20 01:34:03 +0100

    backend

--- backend.sh
+++ backend.sh
@@ -65,3 +65,24 @@ cd /app &>>$LOG_FILE
 rm -rf /app/*  #removes the existing code.
 unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting the backend application code"
+
+npm install &>>LOG_FILE
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+#Load the schema before running backend
+
+dnf install mysql -y &>>LOG_FILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h mysql.naveenganney.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>LOG_FILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable backend &>>LOG_FILE
+VALIDATE $? "Enabled backend"
+
+systemctl start backend &>>LOG_FILE
+VALIDATE $? "restarted backend"
